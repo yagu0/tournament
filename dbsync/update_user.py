8a5c6817961b629893e5a9e495256c561decdc17
Benjamin Auder <benjamin.auder@somewhere>
2021-05-16 19:29:44 +0200

    Fix update_users.py

--- dbsync/update_user.py
+++ dbsync/update_user.py
@@ -10,20 +10,22 @@ def sync_tournament():
     Synchronize tournament/Users from vchess/Users : update
     """
 
+    uid = sys.argv[1]
+
     vconn = create_connection(vchess_db_path)
     vcur = vconn.cursor()
-    vcur.execute("SELECT name,email FROM Users WHERE id = ?", (sys.argv[1],))
+    vcur.execute("SELECT name,email FROM Users WHERE id = ?", (uid,))
     vrow = vcur.fetchone()
     vcur.close()
 
     tconn = create_connection(tournament_db_path)
     tcur = tconn.cursor()
-    tcur.execute("SELECT name,email FROM Users WHERE id = ?", (sys.argv[1],))
+    tcur.execute("SELECT name,email FROM Users WHERE id = ?", (uid,))
     trow = tcur.fetchone()
     if trow[0] != vrow[0]:
-        tconn.cursor().execute("UPDATE Users SET name = ? WHERE id = ?", (vrow[1],vrow[0]))
+        tconn.cursor().execute("UPDATE Users SET name = ? WHERE id = ?", (vrow[0],uid))
     if trow[1] != vrow[1]:
-        tconn.cursor().execute("UPDATE Users SET email = ? WHERE id = ?", (vrow[2],vrow[0]))
+        tconn.cursor().execute("UPDATE Users SET email = ? WHERE id = ?", (vrow[1],uid))
     tcur.close()
     tconn.commit()
 
